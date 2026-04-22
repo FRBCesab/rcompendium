889b9b2eb915dfa0b2154bec315709537ba76a67
Nicolas Casajus <10423581+ahasverus@users.noreply.github.com>
2026-04-22 16:55:34 +0200

    feat: implement generic add_github_action() & utils functions

---
+++ R/get_available_gh_actions.R
@@ -0,0 +1,29 @@
+#' List available GitHub Actions
+#'
+#' @description
+#' This function returns a list of the name of all available GitHub Actions.
+#' This is particularly useful to get the right spelling of the GitHub Action
+#' to be passed to [add_github_action()].
+#'
+#' @return A `character` vector with the name of the available GitHub Actions in
+#' `rcompendium`.
+#'
+#' @export
+#'
+#' @family utilities functions
+#'
+#' @examples
+#' get_available_gh_actions()
+
+get_available_gh_actions <- function() {
+  actions <- gh::gh(
+    endpoint = template_repo_url(),
+    .send_headers = c(
+      `Accept` = "application/vnd.github.raw+json",
+      `Content-Type` = "application/json"
+    )
+  )
+
+  actions <- unlist(lapply(actions, function(x) x[["name"]]))
+  gsub("\\.(yaml|yml)$", "", actions)
+}

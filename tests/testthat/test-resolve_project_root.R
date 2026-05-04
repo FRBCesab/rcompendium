dcc90d7e28478325cac6d2b221a825fd389014b3
Nicolas Casajus <10423581+ahasverus@users.noreply.github.com>
2026-05-04 11:40:56 +0200

    ci: fix path issue on Windows (2)

--- tests/testthat/test-resolve_project_root.R
+++ tests/testthat/test-resolve_project_root.R
@@ -11,88 +11,88 @@ test_that("resolve_project_root() - In project", {
   with_local_project({
     initialize_project(quiet = TRUE)
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     file.create(".here")
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     create_dummy_desc_file()
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     dir.create(".git")
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     dir.create(".vscode")
     file.create(file.path(".vscode", "settings.json"))
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     content <- "\"Packages\": { }"
     writeLines(content, "renv.lock")
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     content <- "Version: 0.0.0"
     writeLines(content, "pkgtest.Rproj")
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     file.create("_pkgdown.yaml")
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     file.create("_pkgdown.yml")
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     file.create("_quarto.yml")
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     file.create(".projectile")
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     file.create("_targets.R")
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     file.create("remake.yml")
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 
   with_local_project({
     dir.create(".drake")
 
-    expect_equal(resolve_project_root(), normalizePath(getwd()))
+    expect_equal(normalizePath(resolve_project_root()), normalizePath(getwd()))
   })
 })
 
@@ -105,7 +105,7 @@ test_that("resolve_project_root() - In subfolder", {
     dir.create("dummy")
     withr::local_dir("dummy")
 
-    expect_equal(resolve_project_root(), wd)
+    expect_equal(normalizePath(resolve_project_root()), wd)
   })
 
   with_local_project({
@@ -115,6 +115,6 @@ test_that("resolve_project_root() - In subfolder", {
     dir.create("dummy/dumby", recursive = TRUE)
     withr::local_dir("dummy/dumby")
 
-    expect_equal(resolve_project_root(), wd)
+    expect_equal(normalizePath(resolve_project_root()), wd)
   })
 })

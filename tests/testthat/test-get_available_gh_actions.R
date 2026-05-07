d5e47e89573a8fbf2841ae9acf057bf3e9da413d
Nicolas Casajus <10423581+ahasverus@users.noreply.github.com>
2026-05-07 11:42:43 +0200

    test: add tests (actions)

---
+++ tests/testthat/test-get_available_gh_actions.R
@@ -0,0 +1,12 @@
+## get_available_gh_actions() ----
+
+test_that("get_available_gh_actions() works", {
+  vcr::use_cassette("get_available_gh_actions_success", {
+    templates <- get_available_gh_actions()
+  })
+
+  expect_true(all(!grepl("\\.ya?ml$", templates)))
+  expect_true(length(templates) > 0)
+  expect_true("test-coverage" %in% templates)
+  expect_false("dependabot" %in% templates)
+})
--- tests/testthat/test-stop_if_invalid_issue_template_name.R
+++ tests/testthat/test-stop_if_invalid_issue_template_name.R
@@ -5,12 +5,6 @@ test_that("stop_if_invalid_issue_template_name() works", {
     expect_invisible(
       stop_if_invalid_issue_template_name("bug_report")
     )
-
-    # Nom invalide
-    expect_error(
-      stop_if_invalid_issue_template_name("this_template_does_not_exist"),
-      "The issue template 'this_template_does_not_exist' is not available"
-    )
   })
 })
 
@@ -19,7 +13,11 @@ test_that("stop_if_invalid_issue_template_name() errors", {
   vcr::use_cassette("stop_if_invalid_issue_template_name_error", {
     expect_error(
       stop_if_invalid_issue_template_name("invalid_issue"),
-      "The issue template 'invalid_issue' is not available"
+      paste0(
+        "The issue template 'invalid_issue' is not available. Please run ",
+        "`get_available_issue_templates()` to list available Issue Templates."
+      ),
+      fixed = TRUE
     )
   })
 })
